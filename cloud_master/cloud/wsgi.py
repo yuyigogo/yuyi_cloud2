7db3f0068f4da26fed3d5f474d3c078d8bee8cfc
lufangfan <lufangfan_py@163.com>
2021-09-27 23:51:17 +0800

    修改uwsgi配置

--- cloud_master/cloud/wsgi.py
+++ cloud_master/cloud/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud.settings')
 
 application = get_wsgi_application()
+sys.path.append("/home/yuyi_cloud2")
