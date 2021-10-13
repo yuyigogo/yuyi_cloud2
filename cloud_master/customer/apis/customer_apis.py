76d01bd70c94d65a65ba66040f6209643b161c81
yuyigogo <ruby_yuyi@163.com>
2021-10-13 23:00:05 +0800

    [master] add get list customers api

--- cloud_master/customer/apis/customer_apis.py
+++ cloud_master/customer/apis/customer_apis.py
@@ -1,5 +1,6 @@
 import logging
 
+from customer.models.customer import Customer
 from customer.services.customer_service import CustomerService
 from customer.validators.customer_serializers import CustomerCreateSerializer
 from rest_framework.status import HTTP_201_CREATED
@@ -14,8 +15,28 @@ logger = logging.getLogger(__name__)
 
 class CustomersView(BaseView):
     permission_classes = PermissionFactory(
-        RoleLevel.CLIENT_SUPER_ADMIN, RoleLevel.CLOUD_SUPER_ADMIN, method_list=("POST",)
+        RoleLevel.CLIENT_SUPER_ADMIN, RoleLevel.CLOUD_SUPER_ADMIN
     )
+
+    def get(self, request):
+        """
+        get list customers, only cloud/client super admin have access to this view.
+        :param request:
+        :return:
+        """
+        user = request.user
+        logger.info(f"{user.username} request list customers")
+        customers = Customer.objects.all()
+        total = customers.count()
+        data = [
+            {
+                "id": str(customer.id),
+                "name": customer.name,
+                "administrative_division": customer.administrative_division,
+            }
+            for customer in customers
+        ]
+        return BaseResponse(data={"total": total, "customers": data})
 
     def post(self, request):
         user = request.user
