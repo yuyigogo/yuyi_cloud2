4b878586fcf7935f693e0cabe2706548b2026180
yuyigogo <ruby_yuyi@163.com>
2021-11-18 13:18:21 +0800

    [master] add sensor list info in one equipment

--- cloud_master/navigation/apis/equipment_navigation_apis.py
+++ cloud_master/navigation/apis/equipment_navigation_apis.py
@@ -1,14 +1,14 @@
+from file_management.models.electrical_equipment import ElectricalEquipment
 from mongoengine import DoesNotExist
+from navigation.services.equipment_navigation_service import SiteNavigationService
 from rest_framework.status import HTTP_404_NOT_FOUND
 
 from common.framework.response import BaseResponse
 from common.framework.view import BaseView
-from file_management.models.electrical_equipment import ElectricalEquipment
-from navigation.services.equipment_navigation_service import SiteNavigationService
 
 
 class EquipmentsNavigationView(BaseView):
-    def get(self, site_id):
+    def get(self, request, site_id):
         """
         all points(sensors) in the corresponding site
         :param site_id:
@@ -18,8 +18,9 @@ class EquipmentsNavigationView(BaseView):
 
 
 class EquipmentNavigationView(BaseView):
-    def get(self, site_id, equipment_id):
+    def get(self, request, site_id, equipment_id):
         """
+        设备下所有测点(传感器)数据列表
         all points(sensors) in the corresponding equipment
         :param site_id:
         :param equipment_id:
--- cloud_master/navigation/services/equipment_navigation_service.py
+++ cloud_master/navigation/services/equipment_navigation_service.py
@@ -1,15 +1,17 @@
+from cloud.models import bson_to_dict
 from cloud.settings import MONGO_CLIENT
-from common.framework.service import BaseService
 from file_management.models.electrical_equipment import ElectricalEquipment
 from file_management.models.measure_point import MeasurePoint
+
+from common.framework.service import BaseService
 
 
 class SiteNavigationService(BaseService):
     @classmethod
     def get_latest_sensor_info(cls, sensor_number: str, sensor_type: str) -> dict:
         mongo_col = MONGO_CLIENT[sensor_type]
-        sensor_data = mongo_col.find_one({"sensor_id": sensor_number, "is_new": True})
-        return sensor_data if sensor_data else {}
+        sensor_data = mongo_col.find_one({"sensor_id": sensor_number, "is_new": True},)
+        return bson_to_dict(sensor_data) if sensor_data else {}
 
     @classmethod
     def get_all_points_in_equipment(cls, equipment: ElectricalEquipment) -> list:
