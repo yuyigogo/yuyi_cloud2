8b6bf54fe4d06ab774af2f9e8fed6e0ec6604693
yuyi03 <yuyi03@qianxin.com>
2022-06-09 15:19:10 +0800

    [master] revert code

--- cloud_master/cloud/asgi.py
+++ cloud_master/cloud/asgi.py
@@ -7,30 +7,43 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-# import os
-#
-# from django.core.asgi import get_asgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud.settings")
-#
-# application = get_asgi_application()
-
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud.settings")
 
-django_application = get_asgi_application()
+application = get_asgi_application()
 
+# import os
+#
+# from channels.routing import ProtocolTypeRouter
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-async def application(scope, receive, send):
-    if scope['type'] == 'http':
-        # Let Django handle HTTP requests
-        await django_application(scope, receive, send)
-    elif scope['type'] == 'websocket':
-        # We'll handle Websocket connections here
-        pass
-    else:
-        raise NotImplementedError(f"Unknown scope type {scope['type']}")
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud.settings')
+#
+# django_application = get_asgi_application()
+#
+#
+# async def application(scope, receive, send):
+#     if scope['type'] == 'http':
+#         # Let Django handle HTTP requests
+#         await django_application(scope, receive, send)
+#     elif scope['type'] == 'websocket':
+#         # We'll handle Websocket connections here
+#         pass
+#     else:
+#         raise NotImplementedError(f"Unknown scope type {scope['type']}")
